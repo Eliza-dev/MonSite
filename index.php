a5e2b76b79ada164fc3b008cfa2b41f7dcd93ab2
Eliza-dev <elisao.lyceehb@gmail.com>
2019-11-04 11:01:12 +0100

    First version WEB site MonSite

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>MonSite GITHUB</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="display-4 text-center text-info"> Découverte de GITHUB</h1><hr>
+    </div> 
+</body>
+</html>
